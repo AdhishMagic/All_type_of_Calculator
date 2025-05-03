914027b1033e67d2f42c9fdcc48768762b3f621a
AdhishMagic <balaadhish.cbe@gmail.com>
2025-05-03 21:26:33 +0530

    Age Calculator

--- Age_Calculator/script.js
+++ Age_Calculator/script.js
@@ -1,28 +1,22 @@
 function calculateAge(){
-
     const today = new Date();
     const birthdateInput = document.getElementById("birthdate").value;
     const birthdateParts = birthdateInput.split("-");   
     const birthDay = birthdateParts[0];
     const birthMonth = birthdateParts[1] - 1; 
-    const birthYear = birthdateParts[2];
-   
+    const birthYear = birthdateParts[2];   
     const birthDate = new Date(birthYear,birthMonth,birthDay);
-
 
     console.log(birthdateInput);
     console.log(birthdateParts);
     console.log(birthDay);
     console.log(birthMonth);
     console.log(birthYear);
-
     const isValidDate = (date) =>{
     return(
         Object.prototype.toString.call(date) === "[object Date]" && !isNaN(date)  
     );
-
     };
-
     if(!isValidDate(birthDate)){
     alert("Invalid Date Format: Please Enter a valid date in DD-MM-YYYY format.");
     return;
@@ -70,16 +64,8 @@ function calculateAge(){
       <p>${ageInseconds} </p>
      </div> 
    `;
-
-
    resultContainer.style.display = "block";
-
 }
-
-
-
-
-
 
 const ageCalculatorForm = document.getElementById("ageCalculator");
 ageCalculatorForm.addEventListener("submit",(event)=>{
