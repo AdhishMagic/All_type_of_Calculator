aa37ab8381e66053f81214fbcaf6bc13f12641bf
AdhishMagic <AdhishMagic@users.noreply.github.com>
2025-11-03 21:07:00 +0530

    update home remove list

--- react-app/src/pages/Home.jsx
+++ react-app/src/pages/Home.jsx
@@ -1,20 +1,8 @@
-import { Link } from 'react-router-dom'
-
 export default function Home(){
   return (
     <div>
       <h1>Welcome to Calci (React)</h1>
-      <p>Choose a calculator:</p>
-      <ul>
-        <li><Link to="/calculator">Calculator</Link></li>
-        <li><Link to="/cash">Cash Calculator</Link></li>
-        <li><Link to="/bmi">BMI Calculator</Link></li>
-        <li><Link to="/age">Age Calculator</Link></li>
-        <li><Link to="/loan">Loan Calculator</Link></li>
-        <li><Link to="/tip">Tip Calculator</Link></li>
-        <li><Link to="/fd">FD Calculator</Link></li>
-        <li><Link to="/expense">Expense Tracker</Link></li>
-      </ul>
+      <p>Use the navigation above to open any calculator.</p>
     </div>
   )
 }
